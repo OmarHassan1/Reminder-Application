c77e429005b284e6333e898cc39b13eb0698d392
OmarHassin <boky555555@gmail.com>
2020-06-10 09:47:02 -0700

    add Sfcookies

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,21 +3,27 @@ import {
   REMOVE_REMINDER,
   CLEAR_REMINDER,
 } from "../actions/types";
+import { bake_cookie, read_cookie } from "sfcookies";
+
 const reminders = (state = [], action) => {
   let reminders = null;
-
+  state = read_cookie("reminders");
   if (action.type === ADD_REMINDER) {
     reminders = [
       ...state,
       { text: action.text, date: action.date, id: Math.random() },
     ];
+    bake_cookie("reminders", reminders);
     console.log("from reminders", reminders);
     return reminders;
   } else if (action.type === REMOVE_REMINDER) {
     reminders = state.filter((reminder) => reminder.id !== action.id);
+    bake_cookie("reminders", reminders);
+    console.log("from reminders", reminders);
     return reminders;
   } else if (action.type === CLEAR_REMINDER) {
     reminders = [];
+    bake_cookie("reminders", reminders);
     console.log("from reducer", reminders);
     return reminders;
   } else {
